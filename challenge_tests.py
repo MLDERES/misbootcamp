f744003fe461ef47f5136e8bfb1ad9de888aee62
Michael Dereszynski <mlderes@hotmail.com>
2020-06-26 13:18:16 -0500

    coding challenge 3 added - are you my mother

--- challenge_tests.py
+++ challenge_tests.py
@@ -15,3 +15,9 @@ def assert_equal(a, b, test_name='', hint=''):
         msg += '\t'+colored(f'{hint}','red','on_white', attrs=['bold'])
     print(msg)
     
+def Test_Relationships():
+    family_a = [("Enid", "Susan"), ("Susan", "Deborah")]
+    family_b = [('Enid', 'Susan'), ('Susan', 'Deborah'), ('Enid', 'Dianne'), ('Dianne', 'Judy'), ('Dianne', 'Fern')]
+
+    relations(family_a,('Enid','Susan'))
+    relations(family_b,('Enid','Judy'))
