086c86ac6a43267e40b31f21ea34b69c214811cd
Michael Dereszynski <mlderes@hotmail.com>
2020-06-28 12:50:23 -0500

    restructured and added coding challenge 4

--- src/challenge_tests.py
+++ src/challenge_tests.py
@@ -14,10 +14,3 @@ def assert_equal(a, b, test_name='', hint=''):
     if not result:
         msg += '\t'+colored(f'{hint}','red','on_white', attrs=['bold'])
     print(msg)
-    
-def Test_Relationships():
-    family_a = [("Enid", "Susan"), ("Susan", "Deborah")]
-    family_b = [('Enid', 'Susan'), ('Susan', 'Deborah'), ('Enid', 'Dianne'), ('Dianne', 'Judy'), ('Dianne', 'Fern')]
-
-    relations(family_a,('Enid','Susan'))
-    relations(family_b,('Enid','Judy'))
