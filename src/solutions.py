ecca386873c8842c77ef279a1fa1a5ed9086092b
Michael Dereszynski <mlderes@hotmail.com>
2020-08-13 15:18:39 +0000

    Update solutions

--- src/solutions.py
+++ src/solutions.py
@@ -11,16 +11,50 @@ def day_of_year(day, month, year):
     year : int
         The year which the day is being calculated
     '''
+    '''
+    The day of the year is 
+        days that have passed in the current month + days in the prior months
+
+    So that April 3, 2015 (non-leap year)
+        3 (3rd day of month 4) + 31 (days in March) + 28 (days in Feb) + 31 (days in Jan)
+    '''
+
+    # Start with total_days set the day of the month requested
+    # for each month prior to the month asked for
+    #   add the number of days in that month
+    # if it's leap year and after Feb,  then add 1.
+    
+    # define the days in every month of the year
     days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    
+    # Start with total_days set the day of the month requested
     total_days = day
+    
+    # for each month prior to the month asked for
+    #   add the number of days in that month
     for m in range(month -1):
         total_days = total_days+days_in_month[m]
+
+    # if it's leap year and after Feb,  then add 1.
     if (month > 2) and is_leap_year(year):
         total_days = total_days + 1
         
     return total_days
 
 def is_leap_year(y):
+    '''
+    Determine whether this is leap year
+
+    Parameters:
+    ----------
+    y : int
+        year to be checked
+
+    Returns:
+    -------
+    bool
+        True if it is a leap year, otherwise False
+    '''
     return (y % 4 == 0) and ((y % 100 != 0) or (y % 400 == 0))
 
 def relations(family_tree, relationship):
@@ -40,8 +74,29 @@ def relations(family_tree, relationship):
         The relationship of the second person in the `relationship` tuple to the first person in the tuple
         
     '''
-    # Replace pass with your code
+    '''
+    ******
+    I know that while mothers can have multiple daughters, each daughter can only have one mother
+    therefore if I store the pair together, then I can lookup the daughters and find out who their mother is
+    from there it's just a matter of looking at the types of relationships available
+    ******
+    '''
+
+    # Create a key:value pair where the child is the key and the parent is the value
+    # Break out the tuple passed in into two variables
+    # Find the mother of each of the names in tuple
+    # Find the grandmother of each relationship in the tuple
+    # If the mother of gen_1 == gen_2 then gen_2 is the MOTHER
+    # If the mother of gen_2 == gen_1 then gen_2 is the DAUGHTER
+    # If the mothers are the same, then they are SISTERS
+    # If the grandmothers are the same, then they are COUSINS
+    # If the grandmother of gen_2 == gen_1 then gen_2 is the GRANDDAUGHTER
+    # If the grandmother of gen_1 == gen_2 then gen_2 is the GRANDMOTHER
+    # If the grandmother of gen_1 = mother of gen_2 then gen_2 is an AUNT
+    # If the mother of gen_1 == the grandmother of gen_2 then gen2 is a NIECE
+
     parents = {}
+
     for parent, child in family_tree:
         # Build a list of children by specifying the parent
         parents[child]  = parent
@@ -50,11 +105,17 @@ def relations(family_tree, relationship):
     gen_1 = relationship[0]
     gen_2 = relationship[1]
     
+    # Find the mother of each of the names in tuple
     gen_1_parent = parents.get(gen_1)
-    gen_1_parent_parent = parents.get(gen_1_parent)
     gen_2_parent = parents.get(gen_2)
+    # Find the grandmother of each relationship in the tuple
+    gen_1_parent_parent = parents.get(gen_1_parent)
     gen_2_parent_parent = parents.get(gen_2_parent)
-    
+
+    # I broke a rule here by `returning` in multiple places
+    #  I've done this for clean code I didn't want a huge 
+    #  if-else tree
+    # Also important - the order of comparisons, because we might have blank == blank
     if gen_2 == gen_1_parent : return 'Mother'
     if gen_2 == gen_1_parent_parent : return 'Grandmother'
     if gen_1 == gen_2_parent : return 'Daughter'
@@ -62,16 +123,22 @@ def relations(family_tree, relationship):
     if gen_1_parent == gen_2_parent : return 'Sister'
     if gen_1_parent_parent == gen_2_parent_parent : return 'Cousin'
     if gen_1_parent_parent == gen_2_parent : return 'Aunt'
-    if gen_1_parent == gen_2_parent_parent : return 'Niece'
+    if gen_1_parent == gen_2_parent_parent : return 'Niece'    
 
-    
 
 def monogram(full_name):
     '''
     Creates a traditional monogram from a supplied full name
     '''
+    # Split the string (on spaces) but only split it twice
     first, middle, last = full_name.split(' ',2)
-    return f'{first[0].lower()}.{last[0].upper()}.{middle[0].lower()}'
+    
+    # Make sure the first/middle initial is lowercase, last is uppercase
+    first_initial = first[0].lower()
+    middle_initial = middle[0].lower()
+    last_initial = last[0].upper()
+
+    return f'{first_initial}.{last_initial}.{middle_initial}'
 
 
 def dispense_cash(amount):
@@ -87,20 +154,85 @@ def dispense_cash(amount):
     int
         The number of bills needed, -1 if it can't be done
     '''
+    '''
+    If the amount isn't divisible by 10, then no need to bother cause it won't work
+    Assuming it is divisible by 10, then let's fulfill the request starting with the 
+    largest bills and moving to smaller bills
+    '''
+    # Get the number of $500 bills that could be used
+    # Total to dispense - (however much we gave in $500) figure out the 100s
+    # Of what's left, how many $50 bills can we give
+    # Again taking what's left, get the max number of 20s
+    # Finally, if there is anything left it must be a 10
     total_bills = 0
     if amount % 10 != 0:
         return -1 # Can't be done, because it has to be a multiple of 10
     
+    # How many $500 bills can we dispense
     b_500 = (amount // 500) # The // operator does integer only division - such that 4 // 3 = 1
-    b_100 = (amount % 500) // 100
-    b_50 = (amount - (b_500*500) - (b_100 *100)) // 50
-    b_20 = (amount - (b_500*500) - (b_100 *100) - (b_50 * 50)) // 20
-    b_10 = (amount - (b_500*500) - (b_100 *100) - (b_50 * 50) - (b_20 * 20)) // 10
+    left_over = amount % 500 # The 'mod' operator says give me the remainder of the division
+    
+    # How many $100 bills can we dispense
+    b_100 = left_over // 100
+    left_over = left_over  % 100
+
+    # How many $50 bills can we dispense
+    b_50 = left_over // 50
+    left_over = left_over % 50
+
+    # How many $20 bills can we dispense
+    b_20 = left_over // 20
+    left_over = left_over % 20
+
+    # How many $10 bills can we dispense
+    b_10 = left_over // 10
     
     total_bills = b_500 + b_100 + b_50 + b_20 + b_10
     return total_bills
 
 
+
+def fruit_calculator(question):
+    # Set the quantity to zero
+    # For each word in the string
+    #   If the word is 'loses', then we are going to be subtracting
+    #   If the word is a number, then convert it to an integer, if the 
+    #   If the previous word was a number (the value of number is not zero),
+    #      then add this word to a dictionary as a key and add the number to the value
+    # Find the last word, remove the '?' and look it up in dictionary - this is the value
+    
+    # Set the qty to zero
+    qty = 0 
+    lose = False
+    fruits = {}
+    words = question.split(' ')
+    # For each word in the string
+    for w in words:
+        
+        # Since the split may include `.` or `,` we need to take these off
+        w = w.rstrip('.').rstrip(',')
+        if w == 'loses':
+            lose = True
+        
+        #   If the word is a number, then figure out if this is a gain, loss or just has
+        if w.isdigit():
+            qty = int(w)
+            # Gains and Loses is 0 if there hasn't been a gain or loss
+            if lose:
+                qty = qty * -1
+                # Need to reset the lose value to False
+                lose = False
+    
+        else:
+            #   If the previous word was a number (the value of qty is not zero),
+            #      then add this word to a dictionary as a key and add the number to the value
+            if qty != 0:
+                fruits[w] = fruits.get(w,0) + qty
+                qty = 0
+    
+    # Find the last word, remove the '?' and look it up in dictionary - this is the value
+    target_fruit = words[-1].rstrip('?')
+    return fruits.get(target_fruit,0)
 
 def test_monogram():
     # Basic tests
@@ -148,48 +280,6 @@ def test_day_of_year():
     assert_equal(day_of_year(15,2,2015),46,'Feb 2, 2015')
     assert_equal(day_of_year(30,6,2020),182,'June 30, 2020', 'Did you check for leap year?')
 
-
-def fruit_calculator(question):
-    # Set the quantity to zero
-    # For each word in the string
-    #   If the word is 'loses', then we are going to be subtracting
-    #   If the word is a number, then convert it to an integer, if the 
-    #   If the previous word was a number (the value of number is not zero),
-    #      then add this word to a dictionary as a key and add the number to the value
-    # Find the last word, remove the '?' and look it up in dictionary - this is the value
-    
-    # Set the qty to zero
-    qty = 0 
-    lose = False
-    fruits = {}
-    words = question.split(' ')
-    # For each word in the string
-    for w in words:
-        
-        # Since the split may include `.` or `,` we need to take these off
-        w = w.rstrip('.').rstrip(',')
-        if w == 'loses':
-            lose = True
-        
-        #   If the word is a number, then figure out if this is a gain, loss or just has
-        if w.isdigit():
-            qty = int(w)
-            # Gains and Loses is 0 if there hasn't been a gain or loss
-            if lose:
-                qty = qty * -1
-                # Need to reset the lose value to False
-                lose = False
-    
-        else:
-            #   If the previous word was a number (the value of qty is not zero),
-            #      then add this word to a dictionary as a key and add the number to the value
-            if qty != 0:
-                fruits[w] = fruits.get(w,0) + qty
-                qty = 0
-    
-    # Find the last word, remove the '?' and look it up in dictionary - this is the value
-    target_fruit = words[-1].rstrip('?')
-    return fruits.get(target_fruit,0)
 
 def test_fruit():
     assert_equal(fruit_calculator('Panda has 8 apples and loses 2 apples.  How many apples?'), 6)
